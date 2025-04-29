0c0e788feb84b1848e7fd6e23c95da0c3b165c17
sumina <suminaawa123@gmail.com>
2025-04-29 12:37:18 +0545

    Calculator Winform

---
+++ classWorks/calculatorWinform/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace calculatorWinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            title = new Label();
+            number1 = new Label();
+            number2 = new Label();
+            result = new Label();
+            number1text = new TextBox();
+            number2text = new TextBox();
+            answerText = new TextBox();
+            add = new Button();
+            subtract = new Button();
+            multiply = new Button();
+            divide = new Button();
+            clear = new Button();
+            SuspendLayout();
+            // 
+            // title
+            // 
+            title.AutoSize = true;
+            title.Location = new Point(329, 30);
+            title.Name = "title";
+            title.Size = new Size(126, 20);
+            title.TabIndex = 0;
+            title.Text = "Simple Calculator";
+            title.Click += label1_Click;
+            // 
+            // number1
+            // 
+            number1.AutoSize = true;
+            number1.Location = new Point(106, 85);
+            number1.Name = "number1";
+            number1.Size = new Size(75, 20);
+            number1.TabIndex = 1;
+            number1.Text = "Number 1";
+            // 
+            // number2
+            // 
+            number2.AutoSize = true;
+            number2.Location = new Point(106, 142);
+            number2.Name = "number2";
+            number2.Size = new Size(75, 20);
+            number2.TabIndex = 2;
+            number2.Text = "Number 2";
+            // 
+            // result
+            // 
+            result.AutoSize = true;
+            result.Location = new Point(257, 227);
+            result.Name = "result";
+            result.Size = new Size(49, 20);
+            result.TabIndex = 3;
+            result.Text = "Result";
+            // 
+            // number1text
+            // 
+            number1text.Location = new Point(203, 83);
+            number1text.Name = "number1text";
+            number1text.Size = new Size(125, 27);
+            number1text.TabIndex = 4;
+            number1text.TextChanged += number1text_TextChanged;
+            // 
+            // number2text
+            // 
+            number2text.Location = new Point(204, 140);
+            number2text.Name = "number2text";
+            number2text.Size = new Size(125, 27);
+            number2text.TabIndex = 5;
+            // 
+            // answerText
+            // 
+            answerText.Location = new Point(312, 224);
+            answerText.Name = "answerText";
+            answerText.ReadOnly = true;
+            answerText.Size = new Size(219, 27);
+            answerText.TabIndex = 6;
+            // 
+            // add
+            // 
+            add.BackColor = Color.AntiqueWhite;
+            add.Location = new Point(556, 60);
+            add.Name = "add";
+            add.Size = new Size(94, 29);
+            add.TabIndex = 7;
+            add.Text = "ADD";
+            add.UseVisualStyleBackColor = false;
+            add.Click += button1_Click;
+            // 
+            // subtract
+            // 
+            subtract.BackColor = Color.AntiqueWhite;
+            subtract.Location = new Point(556, 95);
+            subtract.Name = "subtract";
+            subtract.Size = new Size(94, 29);
+            subtract.TabIndex = 8;
+            subtract.Text = "SUBTRACT";
+            subtract.UseVisualStyleBackColor = false;
+            subtract.Click += subtract_Click;
+            // 
+            // multiply
+            // 
+            multiply.BackColor = Color.AntiqueWhite;
+            multiply.Location = new Point(556, 130);
+            multiply.Name = "multiply";
+            multiply.Size = new Size(94, 29);
+            multiply.TabIndex = 9;
+            multiply.Text = "MULTIPLY";
+            multiply.UseVisualStyleBackColor = false;
+            multiply.Click += multiply_Click;
+            // 
+            // divide
+            // 
+            divide.BackColor = Color.AntiqueWhite;
+            divide.Location = new Point(556, 165);
+            divide.Name = "divide";
+            divide.Size = new Size(94, 29);
+            divide.TabIndex = 10;
+            divide.Text = "DIVIDE";
+            divide.UseVisualStyleBackColor = false;
+            divide.Click += divide_Click;
+            // 
+            // clear
+            // 
+            clear.BackColor = Color.SkyBlue;
+            clear.Location = new Point(38, 30);
+            clear.Name = "clear";
+            clear.Size = new Size(94, 29);
+            clear.TabIndex = 11;
+            clear.Text = "CLEAR";
+            clear.UseVisualStyleBackColor = false;
+            clear.Click += clear_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(clear);
+            Controls.Add(divide);
+            Controls.Add(multiply);
+            Controls.Add(subtract);
+            Controls.Add(add);
+            Controls.Add(answerText);
+            Controls.Add(number2text);
+            Controls.Add(number1text);
+            Controls.Add(result);
+            Controls.Add(number2);
+            Controls.Add(number1);
+            Controls.Add(title);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label title;
+        private Label number1;
+        private Label number2;
+        private Label result;
+        private TextBox number1text;
+        private TextBox number2text;
+        private TextBox answerText;
+        private Button add;
+        private Button subtract;
+        private Button multiply;
+        private Button divide;
+        private Button clear;
+    }
+}
---
+++ classWorks/calculatorWinform/Form1.cs
@@ -0,0 +1,72 @@
+namespace calculatorWinform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            number1text.Text = "";
+            number2text.Text = "";
+            //number1text.KeyPress += number1text_KeyPress;
+            //number2text.KeyPress += number2text_KeyPress;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(number1text.Text);
+            int num2 = int.Parse(number2text.Text);
+            int result = num1 + num2;
+            answerText.Text = result.ToString();
+        }
+
+        private void number1text_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void subtract_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(number1text.Text);
+            int num2 = int.Parse(number2text.Text);
+            int result = num1 - num2;
+            answerText.Text = result.ToString();
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(number1text.Text);
+            int num2 = int.Parse(number2text.Text);
+            int result = num1 * num2;
+            answerText.Text = result.ToString();
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int num1 = int.Parse(number1text.Text);
+                int num2 = int.Parse(number2text.Text);
+                int result = num1 / num2;
+                answerText.Text = result.ToString();
+
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("Cannot divide by zero");
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            number1text.Text = "";
+            //number1text.Clear(); //it is also ok
+            number2text.Text = "";
+            answerText.Text = "";
+        }
+    }
+}
